8448e4123758073ef1986682814af513e2db8eed
LuanKennedy <luanc7459@gmail.com>
2023-05-08 16:08:36 -0300

    useApi Changes

--- freqEscolar/src/hooks/useApi.js
+++ freqEscolar/src/hooks/useApi.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 const useApi = () => {
   async function useGet(url) {
-    const response = await axios.get("https://freqescolar.herokuapp.com/api" + url);
+    const response = await axios.get("http://localhost:8080/api" + url);
     return response;
   }
 
   async function useDelete(url) {
-    const response = await axios.delete("https://freqescolar.herokuapp.com/api" + url);
+    const response = await axios.delete("http://localhost:8080/api" + url);
     return response;
   }
 
   async function usePost(url, data) {
-    const response = await axios.post("https://freqescolar.herokuapp.com/api" + url, data);
+    const response = await axios.post("http://localhost:8080/api" + url, data);
     return response;
   }
 
   async function usePut(url, data) {
-    const response = await axios.put("https://freqescolar.herokuapp.com/api" + url, data);
+    const response = await axios.put("http://localhost:8080/api" + url, data);
     return response;
   }
 
