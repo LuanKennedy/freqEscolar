08205d10e480ef5804ad4858284655ec632e6767
LuanKennedy <luanc7459@gmail.com>
2023-05-08 11:59:44 -0300

    Images system

---
+++ freqEscolar/src/server/app.js
@@ -0,0 +1,32 @@
+import cors from "cors";
+import express, { json } from "express";
+import mongoose from "mongoose";
+import { alunoRouter } from "./controllers/alunoController.js";
+import { disciplinaRouter } from "./controllers/disciplinaController.js";
+import { professorRouter } from "./controllers/professorController.js";
+import { turmaRouter } from "./controllers/turmaController.js";
+
+const app = express();
+
+app.use(json());
+app.use(cors());
+
+app.use("/api/turmas", turmaRouter);
+app.use("/api/professores", professorRouter);
+app.use("/api/disciplinas", disciplinaRouter);
+app.use("/api/alunos", alunoRouter);
+
+const initApplication = async () => {
+  try {
+    await mongoose.connect(
+      "MONGO_URI=mongodb+srv://luan777:jOGABhDpc0igFiCw@cluster0.ctiyqzc.mongodb.net/?retryWrites=true&w=majority"
+    );
+    app.listen(8080, () => {
+      console.log("Rodando.");
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+initApplication();
