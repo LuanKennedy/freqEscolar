e0b22b491d6b33d90f53254503917363157535a4
LuanKennedy <luanc7459@gmail.com>
2023-05-08 19:16:52 -0300

    useApi Changes

--- freqEscolar/src/hooks/useApi.js
+++ freqEscolar/src/hooks/useApi.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 const useApi = () => {
   async function useGet(url) {
-    const response = await axios.get("http://lk-api-falta.henriquehiga.com/api" + url);
+    const response = await axios.get("http://lk-api-falta.henriquehiga.com" + url);
     return response;
   }
 
   async function useDelete(url) {
-    const response = await axios.delete("http://lk-api-falta.henriquehiga.com/api" + url);
+    const response = await axios.delete("http://lk-api-falta.henriquehiga.com" + url);
     return response;
   }
 
   async function usePost(url, data) {
-    const response = await axios.post("http://lk-api-falta.henriquehiga.com/api" + url, data);
+    const response = await axios.post("http://lk-api-falta.henriquehiga.com" + url, data);
     return response;
   }
 
   async function usePut(url, data) {
-    const response = await axios.put("http://lk-api-falta.henriquehiga.com/api" + url, data);
+    const response = await axios.put("http://lk-api-falta.henriquehiga.com" + url, data);
     return response;
   }
 
