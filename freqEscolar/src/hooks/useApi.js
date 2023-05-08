befb37b51a9c132030474e3bbe3dbb3d47f2c66b
LuanKennedy <luanc7459@gmail.com>
2023-05-08 16:16:56 -0300

    vercel changes

--- freqEscolar/src/hooks/useApi.js
+++ freqEscolar/src/hooks/useApi.js
@@ -1,22 +1,22 @@
 import axios from "axios";
 const useApi = () => {
   async function useGet(url) {
-    const response = await axios.get("http://localhost:8080/api" + url);
+    const response = await axios.get("https://freq-escolar1.vercel.app/api" + url);
     return response;
   }
 
   async function useDelete(url) {
-    const response = await axios.delete("http://localhost:8080/api" + url);
+    const response = await axios.delete("https://freq-escolar1.vercel.app/api" + url);
     return response;
   }
 
   async function usePost(url, data) {
-    const response = await axios.post("http://localhost:8080/api" + url, data);
+    const response = await axios.post("https://freq-escolar1.vercel.app/api" + url, data);
     return response;
   }
 
   async function usePut(url, data) {
-    const response = await axios.put("http://localhost:8080/api" + url, data);
+    const response = await axios.put("https://freq-escolar1.vercel.app/api" + url, data);
     return response;
   }
 
